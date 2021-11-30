7e457a1947af979343c2491a0a718c1b31ad6da2
rlb32 <93962719+rlb32@users.noreply.github.com>
2021-11-30 17:23:44 -0600

    Update Items.cpp

--- Items.cpp
+++ Items.cpp
@@ -1,46 +1,41 @@
 #include "Items.h"
 
 Items::Items()
-	:ItemName("")
-	, ItemPrice(0)
-	, ItemQuantity(0)
+	:itemName("")
+	, itemPrice(0)
+	, itemQuantity(0)
 {
 }
 
 
 Items::Items(string iName, float iPrice, int iQuantity)
-	:ItemName(iName)
-	, ItemPrice(iPrice)
-	, ItemQuantity(iQuantity)
+	:itemName(iName)
+	, itemPrice(iPrice)
+	, itemQuantity(iQuantity)
 {
+}
+void Items::ItemNames(std::string iName)
+{
+	itemName = iName;
+}
+string Items::get_item_name()
+{
+	return itemName;
+}
+void Items::ItemPrices(float iPrice)
+{
+	itemPrice = iPrice;
+}
+float Items::get_item_price()
+{
+	return itemPrice;
+}
+void Items::ItemQuantities(int iQuantity)
+{
+	itemQuantity = iQuantity;
+}
+int Items::get_item_quantity()
+{
+	return itemQuantity;
 }
 
-void Items::CalcPrice(float iPrice)
-{
-	ItemPrice = iPrice;
-}
-
-void Items::CalcQuant(int iQuantity)
-{
-	ItemQuantity = iQuantity;
-}
-
-void Items::Name(std::string iName)
-{
-	ItemName = iName;
-}
-
-float Items::enterprice()
-{
-	return ItemPrice;
-}
-
-int Items::enterquant()
-{
-	return ItemQuantity;
-}
-
-string Items::entername()
-{
-	return ItemName;
-}
