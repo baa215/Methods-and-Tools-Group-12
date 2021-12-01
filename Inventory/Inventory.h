224d96434c64e15211302d86f0e4f6b8a794d2f3
rlb32 <93962719+rlb32@users.noreply.github.com>
2021-11-30 18:48:18 -0600

    Add files via upload

---
+++ Inventory/Inventory.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Name: Red Dead Redemption 2 Price : $59.99 Quantity : 14" << endl;
+	cout << "Name : Counterstrike Price : $19.99 Quantity : 20" << endl;
+	cout << "Name : Madden 22 Price : $69.99 Quantity : 18" << endl;
+	cout << "Name : Halo Infinite Price : $69.99 Quantity : 10" << endl;
+	cout << "Name : Call of Duty Vanguard Price : $59.99 Quantity : 3" << endl;
+	cout << "Name : MLB The Show 21 Price : $49.99 Quantity : 30" << endl;
+}
